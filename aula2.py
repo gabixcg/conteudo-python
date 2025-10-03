ef84229751a13231c1770aa9dd3b4f260d0e1e6c
GABRIELLA CERQUEIRA GUALBERTO <gabriella_gualber@estudante.sesisenai.org.br>
2025-10-03 16:48:30 -0300

    Aula 2: variaveis, input e calculos

---
+++ aula2.py
@@ -0,0 +1,24 @@
+# Para criar uma variavel, colocamos:
+# o nome dela
+# o simbolo de atribuição (=)
+# o valor inicial dela
+
+# Uma variavel chamada nome que recebe o meu nome
+nome = "Gabriella"
+print("Olá, me chamo",nome)
+nome = "Anna"
+print("Agora meu nome é",nome)
+print("O tipo da variavel é",type(nome))
+print()
+quantidade_alunos = 16
+print("Estamos em",quantidade_alunos,"alunos na sala")
+print("O tipo de variavel é",type(quantidade_alunos))
+print()
+# Uma variavel do tipo float
+salario_bruto = 1234567890.23
+print("O salário mínimo é R$",salario_bruto)
+print("O tipo de variavel é",type(salario_bruto))
+# Uma variavel do tipo bool
+is_aberta = False
+print("A porta está aberta?",is_aberta)
+print("O tipo de variavel é",type(is_aberta))
